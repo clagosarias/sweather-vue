8d3bedaacef8721bdf8b772dd332fdc30cec9ba3
Cristian L <clagos@odilo.us>
2021-04-05 14:20:57 +0200

    Set up environment with Vite, TS, Jest and Vue-eslint

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import HelloWorld from '/@/components/HelloWorld.vue'
 
 export default {
   name: 'App',
